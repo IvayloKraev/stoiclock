2524ebe5bf352c5aa5236f40bf122307561442d2
IvayloKraev <ivaylokraev@protonmail.com>
2025-05-13 10:53:09 +0300

    Add form for new clocks

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,37 @@ import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
-window.Alpine.start();
+interface Clock {
+	name: string;
+	dateTime: Date;
+}
+
+Alpine.data("newClockForm", () => ({
+	name: null as string | null,
+	date: null as string | null,
+	time: null as string | null,
+	submit() {
+		//TODO: Add validation
+
+		if (!this.name || !this.date || !this.time) {
+			this.clear();
+			return;
+		}
+
+		const dateTime: Date = new Date(this.date + " " + this.time);
+		const clocks: Clock[] = JSON.parse(
+			localStorage.getItem("clocks") || "[]",
+		);
+		clocks.push({ name: this.name, dateTime });
+		localStorage.setItem("clocks", JSON.stringify(clocks));
+
+		this.clear();
+	},
+	clear() {
+		this.name = null;
+		this.date = null;
+		this.time = null;
+	},
+}));
+
+Alpine.start();
